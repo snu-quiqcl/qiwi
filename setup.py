9884ffb781d375562a2ea9d98651427e07a6f3fb
BECATRUE <becatrueyjh@gmail.com>
2024-04-09 13:55:49 +0900

    Update version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("requirements.txt", encoding="utf-8") as f:
 
 setup(
     name="qiwis",
-    version="3.0.1",
+    version="3.1.0",
     author="QuIQCL",
     author_email="kangz12345@snu.ac.kr",
     url="https://github.com/snu-quiqcl/qiwis",
@@ -18,7 +18,7 @@ setup(
     long_description=
         "A framework for integration of PyQt widgets where they can communicate with each other. "
         "This project is mainly developed for trapped ion experiment controller GUI in SNU QuIQCL.",
-    download_url="https://github.com/snu-quiqcl/qiwis/releases/tag/v3.0.1",
+    download_url="https://github.com/snu-quiqcl/qiwis/releases/tag/v3.1.0",
     license="MIT license",
     python_requires=">=3.8",
     install_requires=required,
